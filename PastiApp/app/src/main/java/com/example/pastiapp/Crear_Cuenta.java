3c0a5001cf50d9a3158f1afd264f9de84aaa5688
Oscar F <seaoscar2005@gmail.com>
2025-04-02 21:34:49 -0600

    Nueva Pestaña

--- PastiApp/app/src/main/java/com/example/pastiapp/Crear_Cuenta.java
+++ PastiApp/app/src/main/java/com/example/pastiapp/Crear_Cuenta.java
@@ -1,13 +1,15 @@
 package com.example.pastiapp;
 
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Crear_Cuenta extends AppCompatActivity {
 
@@ -15,5 +17,14 @@ public class Crear_Cuenta extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_cuenta);
+
+        Button btnSiguiente = findViewById(R.id.button3);
+        btnSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Crear_Cuenta.this, Crear_Cuenta2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
