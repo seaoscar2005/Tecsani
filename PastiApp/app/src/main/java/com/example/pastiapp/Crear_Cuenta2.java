37a19d3c476c851a35530688fbcd6d36e48ede7a
Oscar F <seaoscar2005@gmail.com>
2025-04-03 08:22:01 -0600

    Nueva pestaña agregada

--- PastiApp/app/src/main/java/com/example/pastiapp/Crear_Cuenta2.java
+++ PastiApp/app/src/main/java/com/example/pastiapp/Crear_Cuenta2.java
@@ -6,11 +6,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 public class Crear_Cuenta2 extends AppCompatActivity {
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_cuenta2);
+
+        Button btnSiguiente = findViewById(R.id.button4);
+        btnSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Crear_Cuenta2.this, Crear_Cuenta3.class);
+                startActivity(intent);
+            }
+        });
     }
 }
