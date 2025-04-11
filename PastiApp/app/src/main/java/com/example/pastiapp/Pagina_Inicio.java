8089aa08974d96c2820d36f63b32cfe18bd7ce31
Oscar F <seaoscar2005@gmail.com>
2025-04-10 20:44:37 -0600

    Nuevas pestañas añadidas

--- PastiApp/app/src/main/java/com/example/pastiapp/Pagina_Inicio.java
+++ PastiApp/app/src/main/java/com/example/pastiapp/Pagina_Inicio.java
@@ -7,6 +7,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Pagina_Inicio extends AppCompatActivity {
 
@@ -14,5 +18,23 @@ public class Pagina_Inicio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagina_inicio);
+
+        // Boton par ir a mi dispositivo
+        Button btnMiDispositivo = findViewById(R.id.button35);
+        btnMiDispositivo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Pagina_Inicio.this, Mi_Dispositivo.class);
+                startActivity(intent);
+            }
+        });
+        // Botón para ir a Calendario
+        Button btnCalendario = findViewById(R.id.button38);
+        btnCalendario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Pagina_Inicio.this, Calendario.class));
+            }
+        });
     }
 }
